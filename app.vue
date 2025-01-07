270d0bb29dbcf81cf7a468f177ec2ef74e42edcc
ivar-ui <ivanarya990@gmail.com>
2025-01-08 02:35:20 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -23,15 +23,14 @@
       </section>
 
       <section class="mb-12">
-    <div class="bg-white rounded-lg overflow-hidden shadow">
-        <div class="aspect-w-16 aspect-h-9">
-            <video src="/assets/img/bumper1.mp4" 
-                   frameborder="0" 
-                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                   allowfullscreen autoplay loop></video>
+        <div class="bg-white rounded-lg overflow-hidden shadow">
+            <div class="aspect-w-16 aspect-h-9">
+                <video src="/assets/img/bumper1.mp4" frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen autoplay loop></video>
+            </div>
         </div>
-    </div>
-</section>
+    </section>
       
       <section id="skills" class="w-full mb-10">
   <h2 class="text-xl font-semibold mb-4 text-white text-center">Skills</h2> <!-- Ukuran judul lebih kecil -->
